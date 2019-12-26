a371d267ae5e035210ee90f5fe0ff203975cfb74
Leinad <43974923+dabass2@users.noreply.github.com>
2019-12-25 21:43:50 -0600

    hyper scuffed screen capture
    holy hell this is bad LMFAOOOOOO but it captures the screen and audio if it's on a chrome tab and playing already

--- src/background.js
+++ src/background.js
@@ -1,15 +1,96 @@
-global.browser = require('webextension-polyfill')
-import store from './store'
+// global.browser = require('webextension-polyfill')
+// import store from './store'
 
-alert(`Hello ${store.getters.STREAM_STATUS}!`)
+// alert(`Hello ${store.getters.STREAM_STATUS}!`)
 
-var onMessageListener = function(message, sender, sendResponse) {
-    switch(message.type) {
-        case "bglog":
-            console.log(message.obj);
-        break;
-    }
-    return true;
+// var onMessageListener = function(message, sender, sendResponse) {
+//     switch(message.type) {
+//         case "bglog":
+//             console.log(message.obj);
+//         break;
+//     }
+//     return true;
+// }
+
+// chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
+//     const sources = message.sources;
+//     const tab = sender.tab;
+//     chrome.desktopCapture.chooseDesktopMedia(sources, tab, (streamId) => {
+//       if (!streamId) {
+//         sendResponse({
+//           type: 'error',
+//           message: 'Failed to get stream ID'
+//         });
+//       } else {
+//         sendResponse({
+//           type: 'success',
+//           streamId: streamId
+//         });
+//       }
+//     });
+//     return true;
+//   });
+
+  /*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+'use strict';
+
+var errorElement = document.querySelector('#errorMsg');
+var video = document.querySelector('video');
+
+// Put variables in global scope to make them available to the browser console.
+var constraints = window.constraints = {
+  audio: true,
+  video: true
+};
+
+function handleSuccess(stream) {
+  // stream.addTrack(stream.getAudioTracks())
+  // console.log(stream.getTracks())
+  var videoTracks = stream.getVideoTracks();
+  console.log(videoTracks)
+  var audioTracks = stream.getAudioTracks();
+  console.log(audioTracks)
+  // console.log('Got stream with constraints:', constraints);
+  // console.log('Using video device: ' + videoTracks[0].label);
+  stream.oninactive = function() {
+    console.log('Stream inactive');
+  };
+  window.stream = stream; // make variable available to browser console
+  video.srcObject = stream;
+  // displayStream = gDM({ video: true });
+  // audioStream = gUM({ audio: true });
+
+  // displayStream.addTrack(audioStream.getAudioTracks()[0]);
 }
-chrome.runtime.onMessage.addListener(onMessageListener);
 
+function handleError(error) {
+  if (error.name === 'ConstraintNotSatisfiedError') {
+    errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
+        constraints.video.width.exact + ' px is not supported by your device.');
+  } else if (error.name === 'PermissionDeniedError') {
+    errorMsg('Permissions have not been granted to use your camera and ' +
+      'microphone, you need to allow the page access to your devices in ' +
+      'order for the demo to work.');
+  }
+  errorMsg('getDisplayMedia error: ' + error.name, error);
+}
+
+function errorMsg(msg, error) {
+  errorElement.innerHTML += '<p>' + msg + '</p>';
+  if (typeof error !== 'undefined') {
+    console.error(error);
+  }
+}
+
+function gdm() {
+  navigator.mediaDevices.getDisplayMedia(constraints)
+      .then(handleSuccess)
+      .catch(handleError);
+}
