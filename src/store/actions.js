d043b68ca37f10f5ae6c0e1dbcdc875d6f77118e
Nick <rushnett@gmail.com>
2019-12-30 02:34:22 -0600

    add new SHARE_LINK store value

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,3 +3,7 @@ import * as types from './mutation-types'
 export const setStreaming = ({commit}, payload) => {
   commit(types.STREAM_STATUS, payload)
 }
+
+export const setLink = ({commit}, payload) => {
+  commit(types.SHARE_LINK, payload)
+}
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,2 @@
 export const STREAM_STATUS = (state) => state.STREAM_STATUS
+export const SHARE_LINK = (state) => state.SHARE_LINK
